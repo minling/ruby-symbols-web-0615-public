661d852ee8cf77e1c0e0598c09dec5c8facdf2ee
minling <mzhao725@gmail.com>
2015-06-05 14:21:02 -0400

    add answers

--- spec/ruby_symbols_spec.rb
+++ spec/ruby_symbols_spec.rb
@@ -1,10 +1,10 @@
 describe "Object Mutability and Immutability" do
   context 'Strings' do
     # make a string and assign it to this variable
-    my_string = __
+    my_string = "apples"
 
     # make an identical string and assign it to this new variable
-    same_string = __
+    same_string = "apples"
 
     it "has a different object id" do
       expect(my_string.object_id).to_not eq(same_string.object_id)
@@ -19,10 +19,10 @@ describe "Object Mutability and Immutability" do
 
   context 'Symbols' do
     # make a symbol and assign it to this variable
-    my_symbol = __
+    my_symbol = :fruits
 
     # make another symbol that's identical to the above symbol, and save it to a new variable
-    same_symbol = __
+    same_symbol = :fruits
 
     it "has the same object id" do
       expect(my_symbol.object_id).to eq(same_symbol.object_id)
